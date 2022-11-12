00ee2cede28e141f692cb74f36c810356004ccc2
Leobar00 <barbierileon@gmail.com>
2022-11-12 12:20:42 +0100

    Change structure and add transition color for homepage card

--- src/components/CenterCard.tsx
+++ src/components/CenterCard.tsx
@@ -7,18 +7,33 @@ import "swiper/css/pagination";
 import {Mousewheel} from "swiper";
 import { Link } from 'react-router-dom';
 
-const urlImg: {[key:string]:string}= {
-    'fitup' :'/images/HOMEPAGE/FitUp.png',
-    'oceano':'/images/HOMEPAGE/Oceano.png',
-    'bibank':'/images/HOMEPAGE/Bibank.png',
-    'drone' :'/images/HOMEPAGE/Drones.png'
-};
+interface CenterCard {
+    title: string,
+    path : string,
+    color: string
+}
 
+const urlImg: CenterCard[] = [
+    {title:'fitup', path:'/images/HOMEPAGE/FitUp.png',color:'#F19393'},
+    {title:'oceano', path:'/images/HOMEPAGE/Oceano.png',color:'#86E864'},
+    {title:'bibank', path:'/images/HOMEPAGE/Bibank.png',color:'#EBF391'},
+    {title:'drone', path:'/images/HOMEPAGE/Drones.png',color:'#75B3D7'}
+];
+
+
+function changeColorCard(){
+    let key: HTMLElement | null = document.querySelector('.swiper-slide-active');
+    let centerCard: HTMLElement | null = document.querySelector('.center-card')
+    if(key != null && centerCard != null) {
+        let newIndex: number = parseInt(key.dataset.index as string) ?? 4
+        centerCard.style.backgroundColor = urlImg[newIndex].color
+    }
+}
 
 
 const CenterCard = () => {
     return (
-        <div className="center-card">
+        <div className="center-card" style={{backgroundColor: urlImg[3].color}}>
             <Swiper
                 direction={"vertical"}
                 className="mySwiper card"
@@ -26,13 +41,14 @@ const CenterCard = () => {
                 initialSlide={4}
                 mousewheel={true}
                 modules={[Mousewheel]}
+                onSlideChangeTransitionEnd={changeColorCard}
             >
                 {
                     Object.entries(urlImg).map(([key,value]) => {
                         return(
-                            <SwiperSlide className="card">
-                                <Link to={"/" + key}>
-                                    <img src={value} alt=""/>
+                            <SwiperSlide className="card" key={key} data-index={key} >
+                                <Link to={"/" + value.title}>
+                                    <img src={value.path} alt=""/>
                                 </Link>
                             </SwiperSlide>
                         )
