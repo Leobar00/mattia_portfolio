213160d47a4ead65ba6b60dd944fe1c598921b5c
Leobar00 <barbierileon@gmail.com>
2022-12-25 13:31:01 +0100

    Transition center-card for mobile

--- src/components/CenterCard.tsx
+++ src/components/CenterCard.tsx
@@ -39,8 +39,10 @@ function animationCenterCard(e:any)
     if(centerCard != null && imgActive != null) {
         const linkImg: HTMLElement | null    = imgActive.querySelector('.link-img-center');
 
-        centerCard.style.width = '100%'
-        centerCard.style.height = '100%'
+        centerCard.style.width = '100vw'
+        centerCard.style.height = '100vh'
+        centerCard.style.maxHeight = '100vh';
+        centerCard.style.maxWidth = '100vw';
         imgActive.style.animation = 'transition-img-center 2s linear'
         setTimeout(() => {
             window.location.href = linkImg!.getAttribute('href') as string;
