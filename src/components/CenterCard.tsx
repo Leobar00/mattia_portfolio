84b3b3db658e16863a2ecd30a37988a12eb75b2f
Leobar00 <barbierileon@gmail.com>
2022-12-25 14:08:32 +0100

    test for mobile

--- src/components/CenterCard.tsx
+++ src/components/CenterCard.tsx
@@ -43,8 +43,8 @@ function animationCenterCard(e:any)
         imgActive.style.animation = 'transition-img-center 2s linear'
         setTimeout(() => {
             window.location.href = linkImg!.getAttribute('href') as string;
+            centerCard.classList.remove('center-card-animation')
         },1800)
-        centerCard.classList.remove('center-card-animation')
     }
 
 }
