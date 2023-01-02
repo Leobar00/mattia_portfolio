afc2d6c2159767782d6f0a760efb98e1549eb2e7
Leonardo Barbieri <barbierileon@gmail.com>
2023-01-02 14:19:35 +0100

    Mobile fix

--- src/components/CenterCard.tsx
+++ src/components/CenterCard.tsx
@@ -6,20 +6,21 @@ import "swiper/css";
 import "swiper/css/pagination";
 import {Mousewheel} from "swiper";
 import {Link, NavLink, useLocation} from 'react-router-dom';
+import {CSSTransition} from 'react-transition-group';
 
 interface CenterCard {
     title: string,
     path : string,
     color: string,
     firstLine: string,
-    secondLine: string,
+    mobileText: string,
 }
 
 const urlImg: CenterCard[] = [
-    {title:'fitup', path:'/images/HOMEPAGE/FitUp.png',color:'#F19393',firstLine:'FitUp mobile app and website',secondLine:'ux/ui design ux/ui design'},
-    {title:'oceano', path:'/images/HOMEPAGE/Oceano.png',color:'#86E864',firstLine:'Oceano mobile app and website',secondLine:'ux/ui design ux/ui design'},
-    {title:'bibank', path:'/images/HOMEPAGE/Bibank.png',color:'#EBF391',firstLine:'Bibank responsive website',secondLine:'ux/ui design ux/ui design'},
-    {title:'drone', path:'/images/HOMEPAGE/Drones.png',color:'#75B3D7',firstLine:'Drones mobile Drones mobile app',secondLine:'ux/ui design ux/ui design'}
+    {title:'fitup', path:'/images/HOMEPAGE/FitUp.png',color:'#F19393',firstLine:'FitUp mobile app and website',mobileText:'Fitup fitness app'},
+    {title:'oceano', path:'/images/HOMEPAGE/Oceano.png',color:'#86E864',firstLine:'Oceano mobile app and website',mobileText:'Oceano web app'},
+    {title:'bibank', path:'/images/HOMEPAGE/Bibank.png',color:'#EBF391',firstLine:'Bibank responsive website',mobileText:'Bibank responsive website'},
+    {title:'drone', path:'/images/HOMEPAGE/Drones.png',color:'#75B3D7',firstLine:'Drones mobile Drones mobile app',mobileText:'Drones delivery app '}
 ];
 
 
@@ -31,98 +32,108 @@ function changeColorCard(arrayUrlImg: Array<any>)
     let lineTwo : HTMLElement | null = document.querySelector('.container-line .text-line-2 span');
     let lineProgress : HTMLElement | null = document.querySelector('.num-slide .line-progress');
     let firstNum : HTMLElement | null = document.querySelector('.num-slide #first-num');
-    if(key != null && centerCard != null && lineOne != null && lineTwo != null && lineProgress != null && firstNum != null) {
+    let textMobile : HTMLElement | null = document.querySelector('.text-line-mobile > span');
+    if(key != null && centerCard != null && lineOne != null && lineTwo != null && lineProgress != null && firstNum != null && textMobile != null) {
         let newIndex: number = parseInt(key.dataset.index as string) ?? 4;
         centerCard.style.backgroundColor = arrayUrlImg[newIndex].color
         lineOne.innerHTML = arrayUrlImg[newIndex].firstLine
-        lineOne.innerHTML = arrayUrlImg[newIndex].firstLine
         firstNum.innerHTML = '0' + (5 - 1 * (newIndex + 1))
         lineProgress.style.width = 125 - 25 * (newIndex + 1)  + '%';
+        textMobile.innerHTML = arrayUrlImg[newIndex].mobileText
     }
-}
-
-function animationCenterCard(e:any)
-{
-    e.preventDefault();
-    const centerCard: HTMLElement | null = document.querySelector('.center-card');
-    const imgActive: HTMLElement | null  = document.querySelector('.center-card .swiper-slide-active');
-    if(centerCard != null && imgActive != null) {
-        const linkImg: HTMLElement | null= imgActive.querySelector('.link-img-center');
-
-        centerCard.classList.add('center-card-animation');
-        imgActive.style.animation = 'transition-img-center 2s linear';
-        setTimeout(() => {
-            window.location.href = linkImg!.getAttribute('href') as string;
-        },1800)
-    }
-
 }
 
 
 
 const CenterCard = () => {
     const [arrayUrlImg,setUrlImg] = useState(urlImg)
+    const [isEnter,setIsEnter] = useState(false)
+
+    function startAnimation(e:any)
+    {
+        e.preventDefault()
+        setIsEnter((v) => {
+            const imgActive: HTMLElement | null  = document.querySelector('.center-card .swiper-slide-active');
+            if(imgActive != null) {
+                const linkImg: HTMLElement | null = imgActive.querySelector('.link-img-center');
+
+                imgActive.style.animation = 'transition-img-center 2s linear';
+                window.location.href = linkImg!.getAttribute('href') as string;
+            }
+
+            return !v;
+        })
+    }
 
     useEffect(() => {
-        if(window.screen.width < 1024) {
+        if(window.screen.width < 768) {
             setUrlImg([...urlImg].reverse());
         }
     },[])
 
     return (
-        <div className="center-card" style={window.screen.width < 1024 ? {backgroundColor: arrayUrlImg[0].color} : {backgroundColor: arrayUrlImg[3].color}}>
-            <Swiper
-                className="mySwiper card"
-                grabCursor={true}
-                initialSlide={4}
-                mousewheel={true}
-                modules={[Mousewheel]}
-                onSlideChangeTransitionStart={() => changeColorCard(arrayUrlImg)}
-                breakpoints={{
-                    1024: {
-                       direction:'vertical',
-                    },
-                    0 :{
-                        direction:"horizontal",
-                        initialSlide: 0,
-                    }
+        <CSSTransition
+            //1. state (enter/exit)
+            in={isEnter}
+            //2. duration
+            timeout={2000}
+            //3. class name prefix
+            classNames="transition-center"
+        >
+            <div className="center-card" style={window.screen.width < 1024 ? {backgroundColor: arrayUrlImg[0].color} : {backgroundColor: arrayUrlImg[3].color}}>
+                <Swiper
+                    className="mySwiper card"
+                    grabCursor={true}
+                    initialSlide={4}
+                    mousewheel={true}
+                    modules={[Mousewheel]}
+                    onSlideChangeTransitionStart={() => changeColorCard(arrayUrlImg)}
+                    breakpoints={{
+                        1024: {
+                           direction:'vertical',
+                        },
+                        0 :{
+                            direction:"horizontal",
+                            initialSlide: 0,
+                        }
 
-                }}
-            >
-                {
-                    Object.entries(arrayUrlImg).map(([key,value]) => {
-                        return(
-                            <SwiperSlide
-                                className="card"
-                                key={key}
-                                data-index={key}
-                            >
-                                <NavLink className='link-img-center' to={"/" + value.title}>
-                                    <img src={value.path} onClick={animationCenterCard} alt=""/>
-                                </NavLink>
-                            </SwiperSlide>
-                        )
-                    })
-                }
-            </Swiper>
-            <div className="instruction desktop">
-                <p>Drag and press to view projects</p>
-            </div>
-            <div className="container-line-mobile mobile">
-                <div className="line-mobile"></div>
-                <div className="text-line-mobile">
-                    <span>Drones delivery app</span>
+                    }}
+                >
+                    {
+                        Object.entries(arrayUrlImg).map(([key,value]) => {
+                            return(
+                                <SwiperSlide
+                                    className="card"
+                                    key={key}
+                                    data-index={key}
+                                >
+                                    <NavLink className='link-img-center' to={"/" + value.title} >
+                                        <img src={value.path} onClick={startAnimation} alt=""/>
+                                    </NavLink>
+                                </SwiperSlide>
+                            )
+                        })
+                    }
+                </Swiper>
+                <div className="instruction desktop">
+                    <p>Drag and press to view projects</p>
                 </div>
-                <div className="line-mobile"></div>
+                <div className="container-line-mobile mobile">
+                    <div className="line-mobile"></div>
+                    <div className="text-line-mobile">
+                        <span>Drones delivery app</span>
+                    </div>
+                    <div className="line-mobile"></div>
+                </div>
+                <div className="num-slide">
+                    <span id="first-num">01</span>
+                    <span className="container-line-progress">
+                        <span className="line-progress"></span>
+                    </span>
+                    <span>04</span>
+                </div>
             </div>
-            <div className="num-slide desktop">
-                <span id="first-num">01</span>
-                <span className="container-line-progress">
-                    <span className="line-progress"></span>
-                </span>
-                <span>04</span>
-            </div>
-        </div>
+        </CSSTransition>
     )
 }
 export  default CenterCard ;
