32b872ae9a9d506faba7e7cb210bf9eedc188970
Leonardo Barbieri <barbierileon@gmail.com>
2023-03-18 11:57:06 +0100

    Animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 const Navbar = () => {
+
+    setTimeout(() => {
+        document.querySelector('.sidebar-right')?.classList.remove('d-none');
+    },2000)
+
     return (
-      <Link to="/menu" className="sidebar-right animate__animated animate__fadeIn animate__slow">
+      <Link to="/menu" className="sidebar-right animate__animated animate__fadeIn animate__slower d-none">
           <div className="circle"></div>
       </Link>
 
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -7,28 +7,35 @@ import Navbar from './../components/Navbar'
 const Main = () => {
 
     setTimeout(() => {
-        document.querySelector('.text-line > span')?.classList.add('animation-line-1')
-        document.querySelector('.text-line-2 > span')?.classList.add('animation-line-2')
-    },3000)
+        document.querySelector('.text-line > span')?.classList.add('animation-line-1');
+        document.querySelector('.text-line-2 > span')?.classList.add('animation-line-2');
+    },5000)
+
+    setTimeout(() => {
+        document.querySelector('.container-main')?.classList.remove('d-none');
+        document.querySelector('.background-container')?.classList.remove('vw-100');
+    },2000)
 
     return (
         <div className="general-container">
-            <div className="container-main">
-                <Logo />
-                <CenterCard />
-                <div className="container-line desktop">
-                    <div className="line"></div>
-                    <div className="text-line animate__animated animate__slideInLeft animate__slow">
-                        <span>Drones mobile drones mobile app</span>
+            <div className="background-container vw-100 ">
+                <div className="container-main d-none">
+                    <Logo />
+                    <CenterCard />
+                    <div className="container-line desktop">
+                        <div className="line"></div>
+                        <div className="text-line animate__animated animate__slideInLeft animate__slower">
+                            <span>Drones mobile drones mobile app</span>
+                        </div>
+                        <div className="line animate__animated animate__slideInLeft animate__slower"></div>
+                        <div className="text-line-2 animate__animated animate__slideInRight animate__slower">
+                            <span>ux/ui design</span>
+                        </div>
+                        <div className="line animate__animated animate__slideInRight animate__slower"></div>
                     </div>
-                    <div className="line animate__animated animate__slideInLeft animate__slow"></div>
-                    <div className="text-line-2 animate__animated animate__slideInRight animate__slow">
-                        <span>ux/ui design</span>
-                    </div>
-                    <div className="line animate__animated animate__slideInRight animate__slow"></div>
                 </div>
+                <Navbar />
             </div>
-            <Navbar />
         </div>
 
 )
