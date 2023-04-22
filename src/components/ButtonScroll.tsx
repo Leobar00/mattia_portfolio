47399eed5b64bc8440da5e786d4a1d19b8e3a8f9
Leonardo Barbieri <barbierileon@gmail.com>
2023-04-22 14:13:03 +0200

    Reset font-size to previous size

--- src/components/ButtonScroll.tsx
+++ src/components/ButtonScroll.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
+const handleScrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+    console.log('ciao')
+}
 const ButtonScroll = () => {
-    const scrollToTop = () => {
-        document.querySelector('#root')?.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-        console.log('ciao')
-    }
 
     return (
-        <div className="arrow-up" onClick={scrollToTop}>
+        <div className="arrow-up" onClick={handleScrollToTop}>
             <p>&#8963;</p>
         </div>
     );
